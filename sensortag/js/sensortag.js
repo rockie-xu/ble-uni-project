7557d8115abdb8cb08757721d111967f660f3ed3
rockie-xu <klenovyj7list@gmail.com>
2018-06-17 16:30:32 +0200

    17-06-2018 v4

--- sensortag/js/sensortag.js
+++ sensortag/js/sensortag.js
@@ -4,34 +4,43 @@ let battery = null;
 
 function connect() {
     console.log('Requesting Bluetooth Device...');
-    document.getElementById('batteryLevel').innerHTML = "waiting for response ... ";
+    document.getElementById('log').innerHTML = 'Requesting Bluetooth Device...';
     navigator.bluetooth.requestDevice(
         {
             filters: [{services: ['battery_service']}]
         })
         .then(device => {
             console.log('> Found ' + device.name);
-            console.log('Connecting to GATT Server...');
-            device.addEventListener('gattserverdisconnected', onDisconnected)
+            document.getElementById('deviceName').innerText = device.name;
+            document.getElementById('log').innerHTML = 'Connecting to Device...';
+            //device.addEventListener('gattserverdisconnected', onDisconnected)
             return device.gatt.connect();
         })
         .then(server => {
-            console.log('Getting Service 0xffe5 - Light control...');
+            console.log('Getting Battery Service');
+            document.getElementById('log').innerHTML = 'Connecting to Battery Server...';
             return server.getPrimaryService('battery_service');
         })
         .then(service => {
             console.log('Getting Battery Characteristic');
+            document.getElementById('log').innerHTML = 'Getting Battery Characteristic';
             return service.getCharacteristic('battery_level');
         })
         .then(characteristic => {
-            console.log('Reading Battery Level...');
+            document.getElementById('log').innerHTML = 'Reading Battery Level...';
             console.log('All ready!');
             battery = characteristic.readValue();
-            //log('> Battery Level is ' + battery.getUint8(0) + '%');
-            document.getElementById('batteryLevel').innerHTML = "> Battery Level is " + battery.getUint8(0) + "%";
+            document.getElementById('log').innerHTML = battery;
+            console.log('Battery Level is ' + battery.getUint8(0));
+            document.getElementById('batteryLevel').innerHTML = " Battery Level: " + battery.getUint8(0);
             //onConnected();
         })
         .catch(error => {
             console.log('Argh! ' + error);
         });
+}
+
+// Install service worker - for offline support
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js');
 }
