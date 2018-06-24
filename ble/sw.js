f2025589a82bea9597927eb8e20125fe1f131ecd
rockie-xu <klenovyj7list@gmail.com>
2018-06-24 10:48:14 +0200

    24-06-2018 v3.1

--- ble/sw.js
+++ ble/sw.js
@@ -13,6 +13,7 @@ var filesToCache = [
 ];
 
 self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] ...update');
     console.log('[ServiceWorker] Install');
     e.waitUntil(
         caches.open(cacheName).then(function(cache) {
