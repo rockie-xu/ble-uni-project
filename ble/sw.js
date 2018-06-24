fb878fd6e7474f6d56a93543b912721de67f4889
rockie-xu <klenovyj7list@gmail.com>
2018-06-24 08:51:54 +0200

    24-06-2018 v1.1

--- ble/sw.js
+++ ble/sw.js
@@ -1,8 +1,35 @@
-importScripts('sw-toolbox.js');
+var cacheName = 'blue-app';
+var filesToCache = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/icon.png',
+    '/info.html',
+    '/info.css',
+    '/bulb/bulb.html',
+    '/bulb/bulb.js',
+    '/bulb/styles.css',
+    '/bulb/images/icon.png'
+];
 
-toolbox.precache([
-  'companion.js',
-  'index.html',
-  'main.js',
-  'styles.css',
-]);
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('activate',  event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request, {ignoreSearch:true}).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
