4839902ad386ac0a76fe560d3e3656c9100de23d
rockie-xu <klenovyj7list@gmail.com>
2018-06-26 22:12:03 +0200

    26-06 v1.2

--- ble/sw.js
+++ ble/sw.js
@@ -43,10 +43,20 @@ self.addEventListener('activate', function(event) {
     );
 });
 */
+/*
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request, {ignoreSearch:true}).then(response => {
             return response || fetch(event.request);
+        })
+    );
+}); */
+
+self.addEventListener('fetch', function(e) {
+    console.log('[ServiceWorker] Fetch', e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
         })
     );
 });
