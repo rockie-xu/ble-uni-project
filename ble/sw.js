2a7bb4568dcac6cf309cacc4894cd7c04059b145
rockie-xu <klenovyj7list@gmail.com>
2018-06-27 15:13:15 +0200

    27-06 hopefully still correct

--- ble/sw.js
+++ ble/sw.js
@@ -1,19 +1,19 @@
-var cacheName = 'blue-app';
+var cacheName = 'blue-app-3-1';
+var dataCacheName = 'blue-app-v3.1';
 var filesToCache = [
     '/',
-    '/ble/index.html',
-    '/ble/styles.css',
-    '/ble/icon.png',
-    '/ble/info.html',
-    '/ble/info.css',
-    '/ble/bulb/bulb.html',
-    '/ble/bulb/bulb.js',
-    '/ble/bulb/styles.css',
-    '/ble/bulb/images/icon.png'
+    '/index.html',
+    '/styles.css',
+    '/icon.png',
+    '/info.html',
+    '/info.css',
+    '/bulb/bulb.html',
+    '/bulb/bulb.js',
+    '/bulb/styles.css',
+    '/bulb/images/icon.png'
 ];
 
 self.addEventListener('install', function(e) {
-    console.log('[ServiceWorker] ...update again');
     console.log('[ServiceWorker] Install');
     e.waitUntil(
         caches.open(cacheName).then(function(cache) {
@@ -23,40 +23,69 @@ self.addEventListener('install', function(e) {
     );
 });
 
-self.addEventListener('activate',  event => {
-    event.waitUntil(self.clients.claim());
-});
-/*
-self.addEventListener('activate', function(event) {
-    event.waitUntil(
-        caches.keys().then(function(cache) {
-            return Promise.all(
-                cache.filter(function(filesToCache) {
-                    // Return true if you want to remove this cache,
-                    // but remember that caches are shared across
-                    // the whole origin
-                }).map(function(cacheName) {
-                    return caches.delete(cacheName);
-                })
-            );
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
         })
     );
+    /*
+   * Fixes a corner case in which the app wasn't returning the latest data.
+   * You can reproduce the corner case by commenting out the line below and
+   * then doing the following steps: 1) load app for first time so that the
+   * initial New York City data is shown 2) press the refresh button on the
+   * app 3) go offline 4) reload the app. You expect to see the newer NYC
+   * data, but you actually see the initial data. This happens because the
+   * service worker is not yet activated. The code below essentially lets
+   * you activate the service worker faster.
+   */
+    return self.clients.claim();
 });
-*/
-/*
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request, {ignoreSearch:true}).then(response => {
-            return response || fetch(event.request);
-        })
-    );
-}); */
 
-self.addEventListener('fetch', function(e) {
+/*self.addEventListener('fetch', function(e) {
     console.log('[ServiceWorker] Fetch', e.request.url);
     e.respondWith(
         caches.match(e.request).then(function(response) {
             return response || fetch(e.request);
         })
     );
+}); */
+
+self.addEventListener('fetch', function(e) {
+    console.log('[Service Worker] Fetch', e.request.url);
+    var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+    if (e.request.url.indexOf(dataUrl) > -1) {
+        /*
+         * When the request URL contains dataUrl, the app is asking for fresh
+         * weather data. In this case, the service worker always goes to the
+         * network and then caches the response. This is called the "Cache then
+         * network" strategy:
+         * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
+         */
+        e.respondWith(
+            caches.open(dataCacheName).then(function(cache) {
+                return fetch(e.request).then(function(response){
+                    cache.put(e.request.url, response.clone());
+                    return response;
+                });
+            })
+        );
+    } else {
+        /*
+         * The app is asking for app shell files. In this scenario the app uses the
+         * "Cache, falling back to the network" offline strategy:
+         * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+         */
+        e.respondWith(
+            caches.match(e.request).then(function(response) {
+                return response || fetch(e.request);
+            })
+        );
+    }
 });
